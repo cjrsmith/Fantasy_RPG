c9a2197ee292dc6720b9d4e8a297974219a2e0cf
Connor Smith <cs878@kent.ac.uk>
2022-01-25 19:20:48 +0000

    Changed file structure and created namespaces.

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        void Update()
+        {
+            updateAnimator();
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            GetComponent<NavMeshAgent>().destination = destination;
+        }
+
+        private void updateAnimator()
+        {
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+    }
+
+}
